10abb179a18c59055454829abc22923e7e239f04
Soya Miyoshi <sou@Soyas-MacBook-Air.local>
2019-06-26 10:40:57 +0900

    formatした、いらない関数削った

--- mobile_client/lib/main.dart
+++ mobile_client/lib/main.dart
@@ -5,21 +5,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-/*
-Future<List<Post>> fetchPost() async {
- final response = await http.get('http://localhost:8080/api/v1/event');
-  // final response = await http.get('http://d7ef45de.ngrok.io/api/v1/event');
-  print(response.body);
-
-  if (response.statusCode == 200) {
-//    return Post.fromJson(jsonDecode(response.body));
-      return compute(parseData, response.body);
-  } else {
-    throw Exception('Failed to load post');
-  }
-}*/
-
-
 // 6月25 方針転換（みよし） レスポンスを全てStringで受け取る→Json各々要素のリストにする。ただしこの時List<String>
 // →レンダリング時にStringをPostに変換してお好みの要素を取り出す
 
@@ -29,10 +14,10 @@ Future<List<String>> stringfetchPost() async {
   final response = await http.get('http://localhost:8080/api/v1/event');
   if (response.statusCode == 200) {
 //    return Post.fromJson(jsonDecode(response.body));
-  print(response.body);
-  return response.body.trim().split("\n");
+    print(response.body);
+    return response.body.trim().split("\n");
   } else {
-  throw Exception('Failed to load post');
+    throw Exception('Failed to load post');
   }
 }
 
@@ -48,15 +33,12 @@ List<Post> parseData(String responseBody){git
 */
 
 // この関数はまだサーバーの対応待ちでいいです
-Future<String> createPost(String url, String json_to_send) async{
-  final response =
-  await http.post(url, body: json_to_send);
+Future<String> createPost(String url, String json_to_send) async {
+  final response = await http.post(url, body: json_to_send);
 
   if (response.statusCode == 200) {
-    return('ok');
-  }
-
-  else{
+    return ('ok');
+  } else {
     throw Exception('Failed to create a post');
   }
 }
@@ -95,23 +77,22 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class BodyWidget extends StatefulWidget {
   @override
   BodyWidgetState createState() {
     return new BodyWidgetState();
   }
 }
+
 class BodyWidgetState extends State<BodyWidget> {
-  //List<Post> serverResponse = [new Post(id: 1, title: "cool book", memo: "foo"), new Post(id: 2, title: "awesome book", memo: "bar")];
   List<String> serverResponse = [];
 
- @override
- void initState() {
-   //super.initState();
-   _stringmakeGetRequest();
-   //print(serverResponse);
-//    debugPrint(response.first.title);
- }
+  @override
+  void initState() {
+    //super.initState();
+    _stringmakeGetRequest();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -120,19 +101,12 @@ class BodyWidgetState extends State<BodyWidget> {
       child: new GridView.count(
         crossAxisCount: 2,
         children: List.generate(serverResponse.length, (index) {
-          return stringgetStructuredGridCell(serverResponse[index].replaceAll(r'},', '}'));
+          return stringgetStructuredGridCell(
+              serverResponse[index].replaceAll(r'},', '}'));
         }),
       ),
     );
   }
-
-  /*
-  _makeGetRequest() async {
-    List<Post> response = await fetchPost();
-    setState(() {
-      serverResponse = response;
-    });
-  }*/
 
   _stringmakeGetRequest() async {
     List<String> response = await stringfetchPost();
@@ -141,34 +115,6 @@ class BodyWidgetState extends State<BodyWidget> {
     });
   }
 }
-
-/*
-Card getStructuredGridCell(Post post) {
-  return new Card(
-      elevation: 1.5,
-      child: new Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        verticalDirection: VerticalDirection.down,
-        children: <Widget>[
-          // can add picture here
-          new Padding(
-            padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20),
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                // new Icon(Icons.book),
-                new Image(image: new AssetImage('res/img/book.png'), width: 80, height: 80,),
-//                new Text(post.id.toString()),
-                new Text(post.title, style: TextStyle(fontWeight: FontWeight.bold),),
-                 new Text("Memo: "+ post.memo),
-              ],
-            ),
-          )
-        ],
-      ));
-}
-*/
 
 Card stringgetStructuredGridCell(String one_elm) {
   return new Card(
@@ -185,13 +131,20 @@ Card stringgetStructuredGridCell(String one_elm) {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 // new Icon(Icons.book),
-                new Image(image: new AssetImage('res/img/book.png'), width: 80, height: 80,),
+                new Image(
+                  image: new AssetImage('res/img/book.png'),
+                  width: 80,
+                  height: 80,
+                ),
 //                new Text(post.id.toString()),
                 // 各々の要素をPostクラスに変換した後titleに対応する要素を引っ張ってくる
                 // (正規表現でもいいですが、後で仕様が変わることを考えると
                 // Postクラスに変換した方がいい？
                 // というかここはできれば後でmain関数の外に出したい
-                new Text(Post.fromJson(jsonDecode(one_elm)).title, style: TextStyle(fontWeight: FontWeight.bold),),
+                new Text(
+                  Post.fromJson(jsonDecode(one_elm)).title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
               ],
             ),
           )
