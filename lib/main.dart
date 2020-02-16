dfccc7174dceaf8a4d990ea62053b1a177a37204
SoyaMiyoshi <soya.miyoshi@gmail.com>
2020-02-16 19:14:38 +0900

    ログインフォームの表示完了

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,74 @@
-import 'package:bookbasket/book_list_screen.dart';
-import 'package:bookbasket/user_login_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:bookbasket/public_booklist_screen.dart';
-import 'package:bookbasket/otheruser_booklist_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './register_page.dart';
+import './signin_page.dart';
 import 'package:bookbasket/user_list_screen.dart';
+import 'package:bookbasket/user_login_screen.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Firebase Auth Demo',
+//       home: MyHomePage(title: 'Firebase Auth Demo'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+
+//   final String title;
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   FirebaseUser user;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: <Widget>[
+//           Container(
+//             child: RaisedButton(
+//               child: const Text('Test registration'),
+//               onPressed: () => _pushPage(context, RegisterPage()),
+//             ),
+//             padding: const EdgeInsets.all(16),
+//             alignment: Alignment.center,
+//           ),
+//           Container(
+//             child: RaisedButton(
+//               child: const Text('Test SignIn/SignOut'),
+//               onPressed: () => _pushPage(context, SignInPage()),
+//             ),
+//             padding: const EdgeInsets.all(16),
+//             alignment: Alignment.center,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   void _pushPage(BuildContext context, Widget page) {
+//     Navigator.of(context).push(
+//       MaterialPageRoute<void>(builder: (_) => page),
+//     );
+//   }
+// }
+
+
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +76,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ThinkBookClientApp',
+      title: '技術書について気軽に質問できるWebアプリ',
       debugShowCheckedModeBanner: false,
       home: Stack(
         children: <Widget>[
@@ -34,7 +99,7 @@ class MyApp extends StatelessWidget {
             body: new Container(
               color: Colors.transparent,
               padding: const EdgeInsets.fromLTRB(60.0,60.0,60.0,30.0),
-              child: UserLoginScreen(),
+              child: EmailPasswordForm(),
             ),
           )
 //          BookListScreen(userName: "hellboy",),
---
+++ lib/signin_page.dart
@@ -0,0 +1,356 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+class SignInPage extends StatefulWidget {
+  final String title = 'Registration';
+  @override
+  State<StatefulWidget> createState() => SignInPageState();
+}
+
+class SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          Builder(builder: (BuildContext context) {
+            return FlatButton(
+              child: const Text('Sign out'),
+              textColor: Theme.of(context).buttonColor,
+              onPressed: () async {
+                final FirebaseUser user = await _auth.currentUser();
+                if (user == null) {
+                  Scaffold.of(context).showSnackBar(const SnackBar(
+                    content: Text('No one has signed in.'),
+                  ));
+                  return;
+                }
+                _signOut();
+                final String uid = user.uid;
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text(uid + ' has successfully signed out.'),
+                ));
+              },
+            );
+          })
+        ],
+      ),
+      body: Builder(builder: (BuildContext context) {
+        return ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            EmailPasswordForm(),
+          ],
+        );
+      }),
+    );
+  }
+
+  // Example code for sign out.
+  void _signOut() async {
+    await _auth.signOut();
+  }
+}
+
+class EmailPasswordForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => EmailPasswordFormState();
+}
+
+class EmailPasswordFormState extends State<EmailPasswordForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _success;
+  String _userEmail;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 155.0,
+            child: Image.asset('res/img/logo.png'),
+          ),
+          TextFormField(
+            controller: _emailController,
+            decoration: const InputDecoration(labelText: 'Email'),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: const InputDecoration(labelText: 'Password'),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              onPressed: () async {
+                if (_formKey.currentState.validate()) {
+                  _signInWithEmailAndPassword();
+                }
+              },
+              child: const Text('ログイン'),
+            ),
+          ),
+          // Container(
+          //   alignment: Alignment.center,
+          //   padding: const EdgeInsets.symmetric(horizontal: 16),
+          //   child: Text(
+          //     _success == null
+          //         ? ''
+          //         : (_success
+          //             ? 'Successfully signed in ' + _userEmail
+          //             : 'Sign in failed'),
+          //     style: TextStyle(color: Colors.red),
+          //   ),
+          // )
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  // Example code of how to sign in with email and password.
+  void _signInWithEmailAndPassword() async {
+    final FirebaseUser user = (await _auth.signInWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    ))
+        .user;
+    if (user != null) {
+      setState(() {
+        _success = true;
+        _userEmail = user.email;
+      });
+    } else {
+      _success = false;
+    }
+  }
+}
+
+class _EmailLinkSignInSection extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _EmailLinkSignInSectionState();
+}
+
+class _EmailLinkSignInSectionState extends State<_EmailLinkSignInSection>
+    with WidgetsBindingObserver {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+
+  bool _success;
+  String _userEmail;
+  String _userID;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (state == AppLifecycleState.resumed) {
+      final Uri link = await _retrieveDynamicLink();
+
+      if (link != null) {
+        final FirebaseUser user = (await _auth.signInWithEmailAndLink(
+          email: _userEmail,
+          link: link.toString(),
+        ))
+            .user;
+
+        if (user != null) {
+          _userID = user.uid;
+          _success = true;
+        } else {
+          _success = false;
+        }
+      } else {
+        _success = false;
+      }
+
+      setState(() {});
+    }
+  }
+
+  Future<Uri> _retrieveDynamicLink() async {
+    final PendingDynamicLinkData data =
+        await FirebaseDynamicLinks.instance.retrieveDynamicLink();
+    return data?.link;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: const Text('Test sign in with email and link'),
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+          ),
+          TextFormField(
+            controller: _emailController,
+            decoration: const InputDecoration(labelText: 'Email'),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter your email.';
+              }
+              return null;
+            },
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              onPressed: () async {
+                if (_formKey.currentState.validate()) {
+                  _signInWithEmailAndLink();
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              _success == null
+                  ? ''
+                  : (_success
+                      ? 'Successfully signed in, uid: ' + _userID
+                      : 'Sign in failed'),
+              style: TextStyle(color: Colors.red),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> _signInWithEmailAndLink() async {
+    _userEmail = _emailController.text;
+
+    return await _auth.sendSignInWithEmailLink(
+      email: _userEmail,
+      url: '<Url with domain from your Firebase project>',
+      handleCodeInApp: true,
+      iOSBundleID: 'io.flutter.plugins.firebaseAuthExample',
+      androidPackageName: 'io.flutter.plugins.firebaseauthexample',
+      androidInstallIfNotAvailable: true,
+      androidMinimumVersion: "1",
+    );
+  }
+}
+
+class _AnonymouslySignInSection extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AnonymouslySignInSectionState();
+}
+
+class _AnonymouslySignInSectionState extends State<_AnonymouslySignInSection> {
+  bool _success;
+  String _userID;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          child: const Text('Test sign in anonymously'),
+          padding: const EdgeInsets.all(16),
+          alignment: Alignment.center,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton(
+            onPressed: () async {
+              _signInAnonymously();
+            },
+            child: const Text('Sign in anonymously'),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            _success == null
+                ? ''
+                : (_success
+                    ? 'Successfully signed in, uid: ' + _userID
+                    : 'Sign in failed'),
+            style: TextStyle(color: Colors.red),
+          ),
+        )
+      ],
+    );
+  }
+
+  // Example code of how to sign in anonymously.
+  void _signInAnonymously() async {
+    final FirebaseUser user = (await _auth.signInAnonymously()).user;
+    assert(user != null);
+    assert(user.isAnonymous);
+    assert(!user.isEmailVerified);
+    assert(await user.getIdToken() != null);
+      // Anonymous auth does show up as a provider on Android
+      assert(user.providerData.length == 1);
+      assert(user.providerData[0].providerId == 'firebase');
+      assert(user.providerData[0].uid != null);
+      assert(user.providerData[0].displayName == null);
+      assert(user.providerData[0].photoUrl == null);
+      assert(user.providerData[0].email == null);
+    
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    setState(() {
+      if (user != null) {
+        _success = true;
+        _userID = user.uid;
+      } else {
+        _success = false;
+      }
+    });
+  }
+}
